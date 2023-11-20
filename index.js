c01da03798457cd50a61850fd8411d289bb26430
dazega <dazega97@hotmail.com>
2023-11-20 17:59:23 -0600

    feat: first integration of db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const config = require(__dirname + '/config/config.json');
+const port = process.env.PORT || config.port;
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello world');
