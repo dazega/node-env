cb6a505c5e671793df1d1b696886e10ed0eaa894
dazega <dazega97@hotmail.com>
2023-11-20 18:54:52 -0600

    feat: updated connection to db

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
-const config = require(__dirname + '/config/config.json');
+const config = require('./config/config.json');
+const { connectDB, sequelize } = require('./config/db');
 const port = process.env.PORT || config.port;
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello world');
 });
 
-app.listen(port, () => console.log(`App listening port: ${port}`));
+app.listen(port, async () => {
+    console.log(`App listening port: ${port}`);
+    await connectDB();
+    console.log('Sync db wait a moment...');
+    await sequelize.sync({ force: false }).then(() => {
+        console.log("✅Synced database successfully...");
+    });
+});
